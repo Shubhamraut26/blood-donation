d3421cd3d6277e902ab1e7d48f66a3955f71c437
Shubhamraut26 <shubhamraut3254@gmail.com>
2022-03-26 11:44:58 +0530

    Commited By Shubham

--- blood-donation/src/App.js
+++ blood-donation/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Header } from './components/Header';
 import { Login } from './components/Login';
 import { Home } from './components/Home';
 import { Register } from './components/Register';
@@ -7,15 +8,24 @@ import { Search} from './components/Search';
 import { DCRegister } from './components/DCRegister';
 import { About } from './components/About';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Home />
-      <Register />
-      <Search />
-      <DCRegister />
-      <About />
+      <Header/>
+      
+      <Router>
+        <Switch>
+          <Route path="/home" render={()=><Home />}/>
+          <Route path="/login" render={()=><Login/>}/>
+          <Route path="/register" render={()=><Register/>}/>
+          <Route path="/search" render={()=><Search/>}/>
+          <Route path="/dcregister" render={()=><DCRegister/>}/>
+          <Route path="/about" render={()=><About/>}/>
+        </Switch>  
+      </Router>
     </div>
   );
 }
---
+++ blood-donation/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => 
+
+<div>
+    <h1>Blood Donation Management System</h1>
+</div>
