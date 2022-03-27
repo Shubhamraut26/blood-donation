c23ee9371a015a2d504e9c557467593dffbee6a6
Shubhamraut26 <shubhamraut3254@gmail.com>
2022-03-27 17:51:33 +0530

    Commited By Shubham

--- blood-donation/src/components/Search.js
+++ blood-donation/src/components/Search.js
@@ -1,17 +1,7 @@
 import React from 'react'
 
-<<<<<<< HEAD
-
-export const Search = () =>
-
-    <div>
-        <h1>Hello Search</h1>
-        
-    </div>
-=======
  export const Search = () => 
  
  <div>
      <h1>Hello Search</h1>
  </div>
->>>>>>> 1c8442bc5e99ed2a769d8c63761a6c260f51f2e2
