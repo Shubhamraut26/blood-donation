17fb9ca9879a5f629bbf147c54be3411e24eae43
Shubhamraut26 <shubhamraut3254@gmail.com>
2022-03-27 17:46:14 +0530

    Commited By Shubham

--- blood-donation/src/components/Search.js
+++ blood-donation/src/components/Search.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
- export const Search = () => 
- 
- <div>
-     <h1>Hello Search</h1>
-     <h2>I am Shubham Raut</h2>
- </div>
+
+export const Search = () =>
+
+    <div>
+        <h1>Hello Search</h1>
+        
+    </div>
