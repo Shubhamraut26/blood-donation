7cc04b28fe066966f036e2e7683ab51b1e733c6b
Shubhamraut26 <shubhamraut3254@gmail.com>
2022-03-29 21:11:56 +0530

    commit by shubham

--- blood-donation/src/components/Login.js
+++ blood-donation/src/components/Login.js
@@ -93,28 +93,6 @@ export const Login = () => {
 
 
             </div>
-<<<<<<< HEAD
-            <div className="p-3 m-2 bg-secondary text-white col-5">
-                <form>
-                    <h1>Login</h1><br />
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <button variant="primary" type="submit">
-                        Login
-                    </button>
-                    <Link to="/register">
-                        <button className="ml-4" variant="danger">Not Registered ?</button>
-                    </Link>
-                </form>
-=======
->>>>>>> 5758bd9dcc737b90e37b4dbb743b96779e00250e
             </div>
 
 
