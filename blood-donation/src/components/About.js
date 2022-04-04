2e756d5bd7e02d866ecc2e4b7141f4da71d6e53d
Pravin <er.pravinkamble@gmail.com>
2022-04-04 11:40:46 +0530

    Commit

--- blood-donation/src/components/About.js
+++ blood-donation/src/components/About.js
@@ -4,4 +4,23 @@ import React from 'react'
  
  <div>
      <h1>Hello About</h1>
+     <h3>Blood Donation Management System</h3>
+     <p>The BLOOD DONATION MANAGEMENT SYSTEM is great project.<br></br>
+         This is designed for successful completion of a project on blood Doantion management system.<br></br> 
+         The basic building aim is to provide blood donation service to the people.<br></br>
+          Blood Donation Management System is a Website that is designed to store, retrieve and analyze information concerned with the administrative and inventory management within a blood donation services.<br></br> 
+          This project aims at maintaining all the information pertaining to blood donors, different blood groups available in each blood bank and help them manage in a better way.<br></br>
+         Project aim is to provide transperency in this field, make the process of obtaining blood from a blood bank hassle-free and corruption-free and make the system of Blood Donation Management effective.</p>
+         <hr></hr>
+          <h3>Our Team</h3>
+          <p>1)Shubham Raut <br></br>2)Suraj Pimpalkar <br></br>3)Prachi Yadav <br></br>4) Pravin Kamble</p>
+          <hr></hr>
+          <h3>Our Guide</h3>
+          <p>Mr. Shrinivas Kartik</p>
+          <hr></hr>
+          <h3>Our Institution</h3>
+          <p>C-DAC Hyderabad</p>
+
+          
+
  </div>
